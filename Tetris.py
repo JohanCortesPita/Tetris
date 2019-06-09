e5d4031a9917240fed8d84683ac14ea3c39fbdd1
Johan Cortes <jcortes71058@universidadean.edu.co>
2019-06-09 16:07:29 -0500

    Proyecto Tetris

---
+++ Tetris.py
@@ -0,0 +1,11 @@
+import Random
+
+import pygame
+from pygame import Rect
+import numpy as np
+
+np.array(((0, 1), (1, 1), (1, 0),))
+
+Ancho_Ventana, Altura_Ventana = 500, 601
+Ancho_Cuadricula, Altura_Cuadricula = 300, 600
+Tamano_Titulo = 30
